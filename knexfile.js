7f00193101615a7aa87da366c4eca0d216ab0ff3
mandy-kate <mandykatebaker@gmail.com>
2017-04-04 16:50:43 +1200

    set up express and knexfile

---
+++ knexfile.js
@@ -0,0 +1,44 @@
+// Update with your config settings.
+require('dotenv').config();
+let dbConnection = `${ process.env.DATABASE_URL }?ssl=true`;
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: dbConnection
+  },
+
+  staging: {
+    client: 'pg',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
